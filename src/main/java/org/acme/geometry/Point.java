1f9e5351ad7b610b06e5033f0516822074a7cbaa
Camille Blanchon <camille.blanchon@ensg.eu>
2021-01-04 15:27:58 +0100

    Ajout classes Geometry et Point

---
+++ src/main/java/org/acme/geometry/Point.java
@@ -0,0 +1,21 @@
+package org.acme.geometry;
+
+public class Point implements Geometry {
+
+	private Coordinate coordinate;
+	
+	public Point() {
+		this.coordinate = new Coordinate();
+	}
+	
+	public Point(Coordinate coordinate) {
+		this.coordinate = coordinate;
+	}
+	
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
