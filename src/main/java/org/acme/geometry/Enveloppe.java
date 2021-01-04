3eb1ff03fcbeee3457cb78aca17aeb44000a25a0
Camille Blanchon <camille.blanchon@ensg.eu>
2021-01-04 17:08:35 +0100

    squelette enveloppe

---
+++ src/main/java/org/acme/geometry/Enveloppe.java
@@ -0,0 +1,36 @@
+package org.acme.geometry;
+
+public class Enveloppe {
+	private Coordinate bottomLeft;
+	private Coordinate topRight;
+	
+	public Enveloppe() {
+		
+	}
+	
+	public Enveloppe(Coordinate bottomLeft, Coordinate topRight) {
+		this.bottomLeft = bottomLeft;
+		this.topRight = topRight;
+	}
+	
+	public Boolean isEmpty() {
+		
+	}
+	
+	public double getXmax() {
+		
+	}
+	
+	public double getXmin() {
+		
+	}
+	
+	public double getYmax() {
+		
+	}
+	
+	public double getYmin() {
+		
+	}
+
+}
---
+++ src/main/java/org/acme/geometry/EnveloppeBuilder.java
@@ -0,0 +1,13 @@
+package org.acme.geometry;
+
+public class EnveloppeBuilder {
+	
+	public void insert(Coordinate coordinate) {
+		
+	}
+	
+	public Enveloppe build() {
+		return;
+	}
+
+}
