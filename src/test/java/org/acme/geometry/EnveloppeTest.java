19f65d63ee059ef0ddb847f6090b81f2603901d0
Camille Blanchon <camille.blanchon@ensg.eu>
2021-01-05 11:55:11 +0100

    tests enveloppe

--- src/test/java/org/acme/geometry/EnveloppeTest.java
+++ src/test/java/org/acme/geometry/EnveloppeTest.java
@@ -8,33 +8,30 @@ public class EnveloppeTest {
 	public static final double EPSILON = 1.0e-15;
 	
 	@Test
-	public void testBlTrConstructor() {
-		
+	public void testDefaultConstructor() {
+		EnveloppeBuilder builder = new EnveloppeBuilder();
+		Enveloppe result = builder.build();
+		Assert.assertTrue(result.isEmpty());
 	}
 	
-	@Test
-	public void testIsEmpty() {
-		
-	}
 	
 	@Test
-	public void getXmin() {
+	public void testInsertCases() {
+		EnveloppeBuilder builder = new EnveloppeBuilder();
 		
-	}
+		builder.insert(new Coordinate(-1.0, 1.0));
+		builder.insert(new Coordinate(2.0, 0.0));
+		builder.insert(new Coordinate(7.0, 6.0));
+		builder.insert(new Coordinate(1.0, 3.0));
+		builder.insert(new Coordinate(7.0, 2.0));
+		
+		Enveloppe result = builder.build();
+		
+		Assert.assertEquals(7.0, result.getXmax(), EPSILON);
+		Assert.assertEquals(6.0, result.getYmax(), EPSILON);
+		Assert.assertEquals(-1.0, result.getXmin(), EPSILON);
+		Assert.assertEquals(0.0, result.getYmin(), EPSILON);
 	
-	@Test
-	public void getXmax() {
-		
-	}
-	
-	@Test
-	public void getYmin() {
-		
-	}
-	
-	@Test
-	public void getYmax() {
-		
 	}
 	
 	
